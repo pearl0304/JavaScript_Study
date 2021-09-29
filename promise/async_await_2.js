9026741b9af743ff66cdc4953bb20995ce3e5be2
Kylie <kylie@comets.kr>
2021-09-29 16:56:56 +0900

    study : async_await case of resolve

---
+++ promise/async_await_2.js
@@ -0,0 +1,51 @@
+/*
+    Date : Monday September 29 2021
+    Name : Kylie
+    Content : async, await
+
+*/
+
+// CASE - resolve
+function getUserPreferences(callback){
+    const preference = new Promise((resolve, reject)=>{
+        resolve({them : 'apple'})
+    });
+    return preference;
+}
+
+function getMusic(them){
+    if(them === 'apple'){
+        return Promise.resolve({
+            album : 'music for airports'
+        })
+    }
+    return Promise.resolve({
+            album : 'kind of blue'
+    })
+}
+
+function getArtist(album){
+    return Promise.resolve({
+        artist : 'BTS'
+    });
+}
+
+function failMusic(them){
+    return Promise.reject({type : 'ERROR OF NETWORK'})
+}
+
+async function getArtistByPreference(){
+    const {them} = await getUserPreferences();
+    const {album} = await getMusic(them);
+    const {artist} = await getArtist(album);
+
+    return artist;
+}
+
+getArtistByPreference()
+    .then(artist=>{
+        console.log(artist)
+    })
+    .catch(e=>{
+        console.error(e)
+    });
